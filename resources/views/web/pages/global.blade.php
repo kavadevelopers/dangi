1ddf29187a7c640c17600542dcbba2ae748ec6d5
Kava Developers <mehul9921@gmail.com>
2024-02-04 19:41:39 +0530

    Update global.blade.php

--- resources/views/web/pages/global.blade.php
+++ resources/views/web/pages/global.blade.php
@@ -65,7 +65,7 @@
 </section>
 
 
-<section class="bg-light-gray wow animate__fadeIn it-data-section" style="visibility: visible; animation-name: fadeIn;" id="sub-offshoring">
+<section class="bg-light-gray" id="sub-offshoring">
     <div class="container">
         <div class="row row-40 justify-content-lg-between">
             <div class="col-md-6 col-xl-6 text-secondary">
@@ -73,7 +73,7 @@
                     <br>
                     <h3>Traditional Offshoring</h3>
                     <h5 class="text-secondary"><i>Unlock Global Talent, Optimize Costs</i></h5>
-                    <p>In our traditional offshoring model, we offer a dynamic solution where your accounting firm gains access to skilled professionals in India. We operate on a fixed per-hour billing system, ensuring flexibility and efficiency in your workflow. Our team, handpicked to match your specific requirements, seamlessly integrates with your processes, allowing you to focus on core business functions while we handle the rest. Experience the benefits of a global workforce without the complexities.</p>
+                    <p style="font-size:16px;">In our traditional offshoring model, we offer a dynamic solution where your accounting firm gains access to skilled professionals in India. We operate on a fixed per-hour billing system, ensuring flexibility and efficiency in your workflow. Our team, handpicked to match your specific requirements, seamlessly integrates with your processes, allowing you to focus on core business functions while we handle the rest. Experience the benefits of a global workforce without the complexities.</p>
                 </div>
             </div>
             <div class="col-md-6 col-xl-6 text-secondary">
@@ -81,7 +81,7 @@
                     <br>
                     <h3>Transparent Offshoring</h3>
                     <h5 class="text-secondary"><i>Empowering Partnerships, Unveiling Costs</i></h5>
-                    <p>Welcome to a new era of offshoring with our Transparent Offshoring model. We believe in complete openness, ensuring you have a clear understanding of every aspect of your extended team. In this model, we not only place candidates according to your needs but also reveal their salaries to you. We transparently charge for candidate placement, infrastructure, employee responsibilities, and benefits. Whether you prefer a per-hour or weekly billing, our pricing model is straightforward, combining the employee's salary with our fees.</p>
+                    <p style="font-size:16px;">Welcome to a new era of offshoring with our Transparent Offshoring model. We believe in complete openness, ensuring you have a clear understanding of every aspect of your extended team. In this model, we not only place candidates according to your needs but also reveal their salaries to you. We transparently charge for candidate placement, infrastructure, employee responsibilities, and benefits. Whether you prefer a per-hour or weekly billing, our pricing model is straightforward, combining the employee's salary with our fees.</p>
                 </div>
             </div>
         </div>
